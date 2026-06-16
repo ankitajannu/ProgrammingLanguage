f76041185014c65d2514f148457d09bfff1719f1
ankitajannu <ankitajannu11@gmail.com>
2026-06-16 18:09:25 +0530

    Add files via upload

---
+++ Python/Size.py
@@ -0,0 +1,11 @@
+import sys 
+
+No = 119837518764398736928309723457698734693     # int
+
+Marks = 90.99   # float
+
+Name = "Rahul"  # str
+
+print(sys.getsizeof(No))
+print(sys.getsizeof(Marks))
+print(sys.getsizeof(Name))
